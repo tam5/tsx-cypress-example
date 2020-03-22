1ec557e8cdfd6b9561472ce2fea0ee154401b535
tam5 <arimiller92@gmail.com>
2020-03-21 21:22:23 -0400

    example with hook

--- cypress/integration/example.tsx
+++ cypress/integration/example.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import 'cypress-react-unit-test';
+import ReactDOM from 'react-dom';
+
+Cypress.on('window:load', win => {
+    // @ts-ignore
+    win.ReactDOM = window.ReactDOM || win.ReactDOM;
+});
+
+function setup(win: any) {
+    if (win.ReactDOM !== ReactDOM) {
+        win.ReactDOM = ReactDOM;
+    }
+}
+
+beforeEach(() => setup(window))
 
 function MyComponent() {
-    return <div>t</div>
+    const [count, setCount] = React.useState(0);
+
+    const handleClick = () => {
+        console.log('clicking works');
+        setCount(count + 1);
+    };
+
+    return <div onClick={handleClick}>Count is: {count}</div>
 }
 
 describe('example', () => {
     it('should pass', () => {
         cy.mount(<MyComponent />)
+
+        cy.contains('Count').click();
     });
 });
